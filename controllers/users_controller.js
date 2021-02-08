f8698ceededaec45b507ac86ffa24128b4327d06
Ranjeet <ranjeetkccc12@gmail.com>
2021-02-08 05:35:51 -0800

    Saving File From Params

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -17,16 +17,49 @@ module.exports.profile = function(req,res){
 }
 
 
-module.exports.update = function(req,res){
+module.exports.update = async function(req,res){
     
-    if(req.user.id == req.params.id){
+//     if(req.user.id == req.params.id){
 
-        User.findByIdAndUpdate(req.params.id, req.body, function(err,user){
+//         User.findByIdAndUpdate(req.params.id, req.body, function(err,user){
+//             return res.redirect('back');
+//         });
+//     }else{
+//           req.flash('error','Unautherized');
+//         return res.status(401).send('Unauthorized');
+//     }
+
+     if(req.user.id == req.params.id){
+
+        try{
+
+            let user = await User.findByIdAndUpdate(req.params.id);
+            User.uploadedAvatar(req,res,function(err){
+                if(err){console.log('******Multer Error:',err)}
+
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if(req.file){
+                    
+                    //this is saving the path of uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+
+        }catch(err){
+            req.flash('error',err);
             return res.redirect('back');
-        });
-    }else{
-        return res.status(401).send('Unauthorized');
-    }
+        }
+
+    }else{       
+            req.flash('error','Unautherized');
+            return res.status(401).send('Unauthorized');
+        }
+
+
 }
 
 
@@ -34,7 +67,7 @@ module.exports.update = function(req,res){
 module.exports.contact = function(req,res){
     return res.end('<h1>user contact </h1>');
 }
-
+//yes y yes what
 
 //render the signup page
 module.exports.signUp = function(req,res){
