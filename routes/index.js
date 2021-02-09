904d60df335e183858530ff80e3074b1c2978f5e
Ranjeet <ranjeetkccc12@gmail.com>
2021-02-09 08:11:38 -0800

    setting up Directory structure And Rendering an API

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,17 @@ const homeController = require('../controllers/home_controller');
 console.log('routes loaded');
 
 router.get('/',homeController.home);
+
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts')); 
 router.use('/comments',require('./comment'));
 
 
+router.use('/api',require('./api'));
 
+
+//for any further routes access from here
+//router.use('/routerName',require('./routerfile'));
 
 
 module.exports = router;
