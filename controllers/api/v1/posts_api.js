d53905071e23a741067cd5ebc2e0fb13546915a4
Ranjeet <ranjeetkccc12@gmail.com>
2021-02-12 07:50:59 -0800

    AUTHENTICATION AND AUTHORIZATION

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -30,7 +30,7 @@ module.exports.destroy = async function(req,res){
     try{
           
         let post = await Post.findById(req.params.id);
-       // if(post.user == req.user.id){
+       if(post.user == req.user.id){
                   
             post.remove();
     
@@ -41,11 +41,12 @@ module.exports.destroy = async function(req,res){
                
                return res.json(200,{
                    message:'Post and associated comments deleted successfully'
-               })
-        //   }else{
-        //       req.flash('error','You Can Not Delete This Post');
-        //       return res.redirect('back');
-        //   }
+               });
+          }else{
+                 return res.json(401,{
+                     message:"You can not delete this post"
+                 });
+          }
 
     }catch(err){
             
