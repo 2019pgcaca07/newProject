69c55f43b3d788ae076ae49939d57d4f31ed101c
Ranjeet <ranjeetkccc12@gmail.com>
2021-02-18 21:03:37 +0530

    using Google Auth::with a link

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,31 @@
 const express = require('express');
-const router =express.Router();
+const router = express.Router();
 const passport = require('passport');
 
+const usersController = require('../controllers/users_controller');
 
-const userController = require('../controllers/users_controller');
+router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
+router.post('/update/:id', passport.checkAuthentication, usersController.update);
 
-
-router.get('/profile/:id',passport.checkAuthentication ,userController.profile);
-router.post('/update/:id',passport.checkAuthentication ,userController.update);
-
-
-
-router.get('/sign-up',userController.signUp);
-router.get('/sign-in',userController.signIn);
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in', usersController.signIn);
 
 
-router.post('/create',userController.create);
+router.post('/create', usersController.create);
 
-//use passport as a middleware to authenticate
-router.post('/create-session',passport.authenticate(
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
     'local',
-    {failureRedirect:'/users/sign-in'},
-),userController.createSession)
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
 
 
-router.get('/sign-out',userController.destroySession);
+router.get('/sign-out', usersController.destroySession);
+
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+
 
 
 
