682ca41a652aad7c1641d9b3dacec810997edf53
Ranjeet <ranjeetkccc12@gmail.com>
2021-03-09 19:37:38 +0530

    user joined

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,36 +1,34 @@
-class ChatEngine {
-  constructor(chatBoxId, userEmail) {
-    this.chatBox = $(`#${chatBoxId}`);
-    this.userEmail = userEmail;
+class ChatEngine{
+  constructor(chatBoxId, userEmail){
+      this.chatBox = $(`#${chatBoxId}`);
+      this.userEmail = userEmail;
 
-    //initiating a connection on which port i've run the socket server
-    //here io is global variable that is available from script of cdnjs which is in home.ejs
-    this.socket = io.connect("http://localhost:5000");
+      this.socket = io.connect('http://localhost:5000');
 
-    if (this.userEmail) {
-      this.connectionHandler();
-    }
+      if (this.userEmail){
+          this.connectionHandler();
+      }
+
   }
 
-  connectionHandler() {
-    let self = this;
+
+  connectionHandler(){
+      let self = this;
+
+      this.socket.on('connect', function(){
+          console.log('connection established using sockets...!');
 
 
-    this.socket.on("connect", function () {
-      console.log("connection established using sockets...!");
+          self.socket.emit('join_room', {
+              user_email: self.userEmail,
+              chatroom: 'codeial'
+          });
+
+          self.socket.on('user_joined', function(data){
+              console.log('a user joined!', data);
+          })
 
 
-      self.socket.emit('join_room', {
-        user_email: self.userEmail,
-        chatroom: 'codeial'
-    });
-
-    self.socket.on('user_joined', function(data){
-        console.log('a user joined!', data);
-    })
-
-    });
+      });
   }
 }
-
-
