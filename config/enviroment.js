6bcba7539b9023cef728b520f184c78b77fa1369
Ranjeet <ranjeetkccc12@gmail.com>
2021-03-13 21:55:07 +0530

    Logging for Production

--- config/enviroment.js
+++ config/enviroment.js
@@ -1,3 +1,15 @@
+const fs = require("fs");
+const rfs = require('rotating-file-stream');
+const path = require("path");
+
+const logDirectory = path.join(__dirname, "../production_logs");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d",
+  path: logDirectory,
+});
+
 const development = {
   name: "development",
   asset_path: "./assets",
@@ -16,8 +28,12 @@ const development = {
   google_client_id:
     "313233209747-dnqmail3j800a2jvsuckqhohodhs7i63.apps.googleusercontent.com",
   google_client_secret: "0FXb5EBWa4xRfJ8jR-1HKMd2",
-  google_call_back_url: "http://localhost:8000/users/auth/google/callback",
+  google_call_back_url: "http://codeial.com/users/auth/google/callback",
   jwt_secret: "codeial",
+  morgan: {
+    mode: "dev",
+    options: { stream: accessLogStream },
+  },
 };
 
 const production = {
@@ -39,8 +55,13 @@ const production = {
   google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
   google_call_back_url: process.env.GOOGLE_CALLBACK_URL,
   jwt_secret: process.env.CODEIAL_JWT_SECRET,
-  
+  morgan: {
+    mode: "combined",
+    options: { stream: accessLogStream },
+  },
 };
 
-module.exports = eval(process.env.CODEIAL_ENVIROMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIROMENT);
-  
+module.exports =
+  eval(process.env.CODEIAL_ENVIROMENT) == undefined
+    ? development
+    : eval(process.env.CODEIAL_ENVIROMENT);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const env = require('./config/enviroment');
+const logger = require('morgan');
 const cookieParser = require("cookie-parser");
 const app = express();
 const port = 7000;
@@ -27,16 +28,21 @@ chatServer.listen(5000);
 console.log('chat server is listening on port 5000');
 const path = require('path');
 
+if(env.name == 'development'){
 
-app.use(
-  sassMiddleware({
-    src: path.join(__dirname,env.asset_path,'scss'),
-    dest: path.join(__dirname,env.asset_path,'css'),
-    debug: true,
-    outputStyle: "extended",
-    prefix: "/css",
-  })
-);
+  app.use(
+    sassMiddleware({
+      src: path.join(__dirname,env.asset_path,'scss'),
+      dest: path.join(__dirname,env.asset_path,'css'),
+      debug: true,
+      outputStyle: "extended",
+      prefix: "/css",
+    })
+  );
+}
+
+
+
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -45,6 +51,8 @@ app.use(express.static(env.asset_path));
 
 //make the uploads path available to the browser
 app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.use(logger(env.morgan.mode,env.morgan.options));
 
 app.use(expressLayouts);
 //extract styles and scripts from sub pages into the layout.
